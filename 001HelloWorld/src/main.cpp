89b0ad0d3b211a6037783cc99754600a4b67c912
mikelig <mikelirazola@gmail.com>
2023-09-16 19:08:18 +0200

    hello world

---
+++ 001HelloWorld/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int var1 = 100;
+    int *ptr_var1 = &var1;
+    int **pptr_var1 = &ptr_var1;
+    // std stand for "Standard Library" and `cout` means "character out" wich is used to print sth in screen
+    // by doing `std::` we can access pany method of standard library
+    //std::cout << "Hello World!"; 
+
+    std::cout << var1;
+    std::cout << "END";
+
+    // wait user to press enter to close the terminal:
+    // std::cin.get();
+
+    
+    return 0;
+}
